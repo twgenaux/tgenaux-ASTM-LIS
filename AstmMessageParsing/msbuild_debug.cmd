510d405f09a440e6dce02a18b7f2ff05667b589e
twgenaux <143005548+twgenaux@users.noreply.github.com>
2024-03-13 08:57:28 -0400

    Added build scripts

---
+++ AstmMessageParsing/msbuild_debug.cmd
@@ -0,0 +1,16 @@
+@echo OFF
+
+REM https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2015/msbuild/msbuild?view=vs-2015
+
+REM Visual Studio MSBUILD path
+@SET MSBUILD="C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
+
+@SET FLAGS=-t:Rebuild -p:Configuration=Debug -noConsoleLogger -fileLoggerParameters:LogFile=msbuild_debug.log;Append;Verbosity=normal;Encoding=UTF-8
+
+@del .\msbuild_debug.log
+
+REM Must be build based on dependencies. Build with Visual Studio and copy the order.
+@%MSBUILD% ..\AstmCommon\AstmCommon.csproj %FLAGS%
+@%MSBUILD% .\AstmMessageParsing\AstmMessageParsing.csproj %FLAGS%
+
+pause
