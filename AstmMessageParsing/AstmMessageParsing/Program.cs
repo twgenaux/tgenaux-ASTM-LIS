79b2b99d9328cac0526409a18ae197309932eeb4
twgenaux <143005548+twgenaux@users.noreply.github.com>
2024-03-27 15:54:15 -0400

    Update Program.cs

--- AstmMessageParsing/AstmMessageParsing/Program.cs
+++ AstmMessageParsing/AstmMessageParsing/Program.cs
@@ -85,52 +85,103 @@ namespace tgenaux.astm
 
             foreach (string path in paths)
             {
-                MessageParsing messageParsing = new MessageParsing();
-
-                messageParsing.OnlyMapped = OnlyMapped; // If true, only exports mapped fields
-
-                if (File.Exists(translationMapPathname))
-                {
-                    messageParsing.TranslationRecordMap = AstmRecordMap.ReadAstmTranslationRecordMap(translationMapPathname);
-                }
-
+                ////// Load the bi-directional translation map
                 AstmRecordMap transMap = new AstmRecordMap();
                 if (File.Exists(translationMapPathname))
                 {
                     transMap = AstmRecordMap.ReadAstmTranslationRecordMap(translationMapPathname);
                 }
 
+                //////////////////////////////////////// Extract the message content
 
+                ////// Parse the message
                 string[] msg = File.ReadAllLines(path);
-
                 List<Dictionary<string, string>> mappedMessage1 = MessageParsing.ParseMessage(msg.ToList(), false, new AstmRecordMap());
 
+                // The message content has been extracted into a list of key-value pairs, 
+                // where the key is the location of the value in each record.
+                // O.1:O
+                // O.2:1
+                // O.3:SID101
+                // O.5:ABO-D
+                // O.6:N
+                // O.7:20240307151207
+                // O.16:CENTBLOOD
+                // O.23:20240307151237
+                // O.26:F
                 DumpMappedMessage(mappedMessage1);
                 Console.WriteLine();
 
+                ////// Translate the message, replacing the location of each value with a token.
                 List<Dictionary<string, string>> mappedMessage2 = AstmRecordMap.RemapRecord(mappedMessage1, transMap, true);
                 DumpMappedMessage(mappedMessage2);
                 Console.WriteLine();
 
+                // The message content is now a list of key-value pairs, 
+                // where the key is token for the value in each record.
+                // O-RecordType:O
+                // O-SeqNumber:1
+                // O-SampleIDs:SID101
+                // O-Profiles:ABO-D
+                // O-Priority:N
+                // O-RequestedTimeStamp:20240307151207
+                // O-SampleTypes:CENTBLOOD
+                // O-ReportedTime:20240307151237
+                // O-ReportType:F
+
+
+                //////////////////////////////////////// Create a new message with the extracted content of the original message
+
+                ////// Translate the message, replacing the token of each value 
+                ////// with the location of the value in each record (O.16:CENTBLOOD).
                 List<Dictionary<string, string>> mappedMessage3 = AstmRecordMap.RemapRecord(mappedMessage2, transMap, true);
                 DumpMappedMessage(mappedMessage3);
                 Console.WriteLine();
 
-                // Round Trip
+                // The message content is now a list of key-value pairs, 
+                // where the key is the location of the value in each record.
+                // O.1:O
+                // O.2:1
+                // O.3:SID101
+                // O.5:ABO-D
+                // O.6:N
+                // O.7:20240307151207
+                // O.16:CENTBLOOD
+                // O.23:20240307151237
+                // O.26:F
+
+                ////// The delimiters to use for this message
                 string delimiters = @"|\^&";
                 string septerators = @"|\^";
                 string escape = @"&";
 
+                ///// Create the message.
                 List<string> roundTripMsg = MessageParsing.CreateMessge(septerators, escape, delimiters, mappedMessage3, false, new AstmRecordMap());
 
+                ////// Display the original message
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine($"{string.Join("\r\n", msg)}");
                 Console.WriteLine();
 
+                ////// Display the new message
                 Console.WriteLine();
                 Console.WriteLine($"{string.Join("\r\n", roundTripMsg)}");
                 Console.WriteLine();
+
+
+                // # Original message
+                // H|\^&|||OCD^VISION^5.10.0.46252^JNumber|||||||P|LIS2-A|20240307151237
+                // P|1|PID123456||NID123456^MID123456^OID123456|Brown^Bobby^B|White|19650102030400|U||||||||||||||||||||||||||
+                // O|1|SID101||ABO-D|N|20240307151207|||||||||CENTBLOOD|||||||20240307151237|||F|||||
+                // L||
+
+                // # New message, notice that there are no trailing delimiters in this message
+                // H|\^&|||OCD^VISION^5.10.0.46252^JNumber|||||||P|LIS2-A|20240307151237
+                // P|1|PID123456||NID123456^MID123456^OID123456|Brown^Bobby^B|White|19650102030400|U
+                // O|1|SID101||ABO-D|N|20240307151207|||||||||CENTBLOOD|||||||20240307151237|||F
+                // L
+
             }
             Console.WriteLine();
 
